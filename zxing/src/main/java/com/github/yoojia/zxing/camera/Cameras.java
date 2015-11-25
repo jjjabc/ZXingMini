ab13c4dfdec7d1f116d85410a53c501d194222e1
msdx <645079761@qq.com>
2015-11-25 17:43:31 +0800

    Fixed bug that the camera couldn't be opened when start() was called again.

--- zxing/src/main/java/com/github/yoojia/zxing/camera/Cameras.java
+++ zxing/src/main/java/com/github/yoojia/zxing/camera/Cameras.java
@@ -17,49 +17,59 @@ public class Cameras {
 
     private final SurfaceView mPreviewSurfaceView;
     private final CameraManager mCameraManager;
+    private boolean mIsSurfaceViewReady = false;
     private final Handler mHandler = new Handler();
-    private final Runnable mTask = new Runnable(){
-        @Override public void run() {
-            Log.d(TAG, "- NOW open camera and start preview...");
-            try {
-                mCameraManager.open();
-            } catch (Exception e) {
-                Log.e(TAG, "- Cannot open camera", e);
-            }
-            mCameraManager.startPreview();
-            try {
-                mCameraManager.attachPreview(mPreviewSurfaceView.getHolder());
-            } catch (IOException e) {
-                Log.e(TAG, "- Cannot attach to preview", e);
-            }
-        }
-    };
+
     private final SurfaceViewReadyCallback mViewReadyCallback = new SurfaceViewReadyCallback() {
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
             if (mCameraManager.isOpen()) {
                 return;
             }
+            mIsSurfaceViewReady = true;
             Log.d(TAG, "- Preview SurfaceView NOW ready, open camera by CameraManager");
-            mTask.run();
+            openCameraAndPreview();
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            super.surfaceDestroyed(holder);
+            mIsSurfaceViewReady = false;
         }
     };
-
-    private int mStartCameraDelay = 10;
 
     public Cameras(SurfaceView previewSurfaceView) {
         mPreviewSurfaceView = previewSurfaceView;
         mCameraManager = new CameraManager(previewSurfaceView.getContext());
     }
 
-    public void start(){
-        Log.d(TAG, "- Try open camera and start preview...");
-        mHandler.postDelayed(mTask, mStartCameraDelay);
+    private void openCameraAndPreview() {
+        Log.d(TAG, "- NOW open camera and start preview...");
+        try {
+            mCameraManager.open();
+        } catch (Exception e) {
+            Log.e(TAG, "- Cannot open camera", e);
+        }
+        mCameraManager.startPreview();
+        try {
+            mCameraManager.attachPreview(mPreviewSurfaceView.getHolder());
+        } catch (IOException e) {
+            Log.e(TAG, "- Cannot attach to preview", e);
+        }
     }
 
-    public void stop(){
+    public void start() {
+        Log.d(TAG, "- Try open camera and start preview...");
+        if (mIsSurfaceViewReady) {
+            openCameraAndPreview();
+        } else {
+            final SurfaceHolder holder = mPreviewSurfaceView.getHolder();
+            holder.addCallback(mViewReadyCallback);
+        }
+    }
+
+    public void stop() {
         Log.d(TAG, "- Try stop preview and close camera...");
-        mHandler.removeCallbacks(mTask);
         final SurfaceHolder holder = mPreviewSurfaceView.getHolder();
         holder.removeCallback(mViewReadyCallback);
         mCameraManager.stopPreview();
@@ -69,13 +79,4 @@ public class Cameras {
             e.printStackTrace();
         }
     }
-
-    /**
-     * 设定在SurfaceView回调无效的情况下,重启相机的延时时间
-     * @param delayMs 延时时间
-     */
-    public void setStartCameraDelay(int delayMs) {
-        mStartCameraDelay = delayMs;
-    }
-
 }
